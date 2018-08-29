444282dee0241bc4f9a039db74a7b5824025a679
Robin Böhm <robin.boehm@workshops.de>
2018-08-29 14:51:59 +0200

    feat(course): insert updated template with responsive design

--- _plugins/fetch_course_reviews.rb
+++ _plugins/fetch_course_reviews.rb
@@ -1,8 +1,7 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
   puts "Fetching course..."
-  #response = RestClient.get('https://workshops.de/api/course/17')
-  response = RestClient.get('http://localhost:4000/api/course/17')
+  response = RestClient.get('https://workshops.de/api/course/17')
   File.write('_data/course.json', response.body)
   puts "Fetching course...done"
 end
--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -1,6 +1,5 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
-  #response = RestClient.get('https://workshops.de/api/course/17/events')
-  response = RestClient.get('http://localhost:4000/api/course/17/events')
+  response = RestClient.get('https://workshops.de/api/course/17/events')
   File.write('_data/events.json', response.body)
 end
