ac6785d752f33a2fd86de91950874deb71c05676
Robin Böhm <robin.boehm@workshops.de>
2019-11-22 12:59:44 +0100

    feat(training): add react native training

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -2,4 +2,7 @@ Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
   response = RestClient.get('https://workshops.de/api/course/17/events')
   File.write('_data/events.json', response.body)
+
+  response = RestClient.get('https://workshops.de/api/course/27/events')
+  File.write('_data/events-react-native.json', response.body)
 end
