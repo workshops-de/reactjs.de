9b17f430294c353acc3857bb5e164d44af5d7f6e
Sascha <mail@sascha.dev>
2021-06-25 16:27:19 +0100

    chore(events): add related events

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -1,8 +1,20 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
-  response = RestClient.get('https://workshops.de/api/course/32/events')
-  File.write('_data/events.json', response.body)
+  if(ENV["JEKYLL_ENV"] != "local") then
+    puts "Fetching events..."
 
-  response = RestClient.get('https://workshops.de/api/course/27/events')
-  File.write('_data/events-react-native.json', response.body)
+    response = RestClient.get('https://workshops.de/api/course/32/events')
+    File.write('_data/events/react.json', response.body)
+
+    response = RestClient.get('https://workshops.de/api/course/32/related-events')
+    File.write('_data/related_events/react.json', response.body)
+
+    response = RestClient.get('https://workshops.de/api/course/27/events')
+    File.write('_data/events/react-native.json', response.body)
+
+    response = RestClient.get('https://workshops.de/api/course/27/related-events')
+    File.write('_data/related_events/react-native.json', response.body)
+
+    puts "Fetching events...done"
+  end
 end
